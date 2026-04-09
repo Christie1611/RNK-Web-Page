17d7c0672d047556dce0a89767a714adf94c4d70
Cuenta Chris <chrislizardo1611@gmail.com>
2026-04-09 23:40:12 +0200

    Contenido a una facción, pensando en los miembros

--- Javascript/funcionesFactions.js
+++ Javascript/funcionesFactions.js
@@ -10,3 +10,18 @@ links.forEach(link => {
         }
     })
 })
+
+const readM = document.querySelector(".readMore");
+const moreT = document.querySelector(".moreText");
+
+readM.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (moreT.style.maxHeight) {
+        moreT.style.maxHeight = null;
+        readM.textContent = "Leer Más (SPOILERS)";
+    } else {
+        moreT.style.maxHeight = moreT.scrollHeight + "px";
+        readM.textContent = "Leer Menos";
+    }
+})
