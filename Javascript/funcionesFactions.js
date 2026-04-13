8b1e11ee15f731898d69bc45e85a3c6a22c0e8a6
Cuenta Chris <chrislizardo1611@gmail.com>
2026-04-13 15:52:36 +0200

    Rellenando informacion y prototipo de miembros en las facciones

--- Javascript/funcionesFactions.js
+++ Javascript/funcionesFactions.js
@@ -1,3 +1,6 @@
+import { characters } from "./personajesFactions.js"
+
+// PARTE DESPLEGABLE DEL MENÚ
 const links = document.querySelectorAll(".menu li > a");
 links.forEach(link => {
     link.addEventListener("click", (e) => {
@@ -9,8 +12,9 @@ links.forEach(link => {
             submenu.classList.toggle("open");
         }
     })
-})
+});
 
+// LEER MÁS
 const readM = document.querySelector(".readMore");
 const moreT = document.querySelector(".moreText");
 
@@ -24,4 +28,25 @@ readM.addEventListener("click", (e) => {
         moreT.style.maxHeight = moreT.scrollHeight + "px";
         readM.textContent = "Leer Menos";
     }
-})
+});
+
+// PERSONAJES DE LAS FACCIONES
+let index = 0;
+function updateCharacter() {
+    document.getElementById("characterName").textContent = characters[index].name;
+    document.getElementById("characterImg").src = characters[index].img ;
+    document.getElementById("characterDesc").textContent = characters[index].desc;
+}
+
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+
+next.addEventListener("click", () => {
+    index = (index + 1) % characters.length;
+    updateCharacter();
+});
+
+prev.addEventListener("click", () => {
+    index = (index - 1 + characters.length) % characters.length;
+    updateCharacter();
+});
---
+++ Javascript/personajesFactions.js
@@ -0,0 +1,16 @@
+export const characters = [
+    {
+        name: "John V. Neumann",
+        img: "../Imagenes/Neumann.png",
+        desc: "Siempre en silla de ruedas, fría y calculadora",
+        talento: "Demonio del Cálculo",
+        talenDesc: "Puede calcular a gran velocidad."
+    },
+    {
+        name: "Isaac Newton",
+        img: "../Imagenes/Neumann.png",
+        desc: "Chico con cabeza de manzana",
+        talento: "Demonio del Cálculo",
+        talenDesc: "Puede calcular a gran velocidad."
+    }
+]
